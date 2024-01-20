060a1919f956a103830d151540cecb30653dc256
Gulnar Kodzhaeva <gulnarkodzhaeva@gmail.co>
2024-01-20 21:51:25 +0300

    ustanovka firebase, ant

---
+++ my-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoRm2-QqGX1O9jnofSvWb33ETA1bQseuo",
+  authDomain: "crmanagementsystemproject.firebaseapp.com",
+  projectId: "crmanagementsystemproject",
+  storageBucket: "crmanagementsystemproject.appspot.com",
+  messagingSenderId: "239107684597",
+  appId: "1:239107684597:web:cd26f63470311f94e9adbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
